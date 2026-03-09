ff258ececf7817e22d8ff03c83611b6378ef6bdf
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-03-09 12:36:38 -0400

    feat: Add basic Ship class implementation

---
+++ src/modules/classes/ship.js
@@ -0,0 +1,39 @@
+export class Ship {
+    static TYPES = new Map([
+        ['carrier', 5],
+        ['battleship', 4],
+        ['destroyer', 3],
+        ['submarine', 3],
+        ['patrolboat', 2],
+    ]);
+
+    #type;
+    #length;
+    #hitCounter;
+
+    constructor(type) {
+        this.type = type;
+        this.#length = Ship.TYPES.get(this.#type);
+        this.#hitCounter = 0;
+    }
+
+    set type(type) {
+        if (type) throw new Error('Cannot re-assign Ship type.');
+
+        const trimmedType = type.toLowerCase().trim();
+        if (!Ship.TYPES.has(trimmedType)) throw new Error(`${trimmedType} is not a valid ship type.`);
+
+        this.#type = trimmedType;
+    }
+    get type() {
+        return this.#type;
+    }
+
+    get length() {
+        return this.#length;
+    }
+    
+    get hitCounter() {
+        return this.#hitCounter;
+    }
+}

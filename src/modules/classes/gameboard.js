f0eda73a05938af116c4e31c0817ae0d1bbf6f59
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-03-10 13:21:11 -0400

    feat: Add placeShip() method and tests for type checking

---
+++ src/modules/classes/gameboard.js
@@ -0,0 +1,51 @@
+export class Gameboard {
+    static isOutOfBounds(coordinate) {
+        const char = coordinate[0];
+        const intPart = parseInt(coordinate.slice(1));
+
+        return (char < 'A' || char > 'J') || (intPart < 1 || intPart > 10);
+    }
+    /**
+     * A Map() that stores Key: Values of the coordinate and it's linked ship in a 
+     * <string, ship> fashion.
+     * 
+     * E.G:
+     * Map.set('A1', destroyer);
+     * Map.set('A2', destroyer);
+     * Map.set('A3', destroyer);
+     * 
+     * So multiple coordinates reference one ship, instead of vice-versa.
+     * 
+     * I can just check if a key exists during ship placement to prevent overlapping, or
+     * get the refernced ship to call hit() on during gameplay.
+     */
+    #shipPlacements;
+
+    /**
+     * A set that stores the players guessed spaces against the other players board.
+     * The set structure prevents the player from making multiple guesses on the same space.
+     */
+    #guessedSpaces;
+
+    constructor() {
+        this.#shipPlacements = new Map();
+        this.#guessedSpaces = new Set();
+    }
+
+    /**
+     * Creates a new Ship object and recordss it's position on the game board.
+     * 
+     * @param {string} coordinate - The coordinates to place the ship into. 
+     */
+    placeShip(coordinate) {
+        if (typeof coordinate !== 'string')
+            throw new TypeError('coordinate must be passed as a string');
+
+        const trimmedCoords = coordinate.toUpperCase().trim();
+        if (!trimmedCoords)
+            throw new Error('coordinates cannot be blank or whitespace');
+
+        if (Gameboard.isOutOfBounds(trimmedCoords))
+            throw new RangeError(`${trimmedCoords} is out of bounds`);
+    }
+}

bd51a7af2ab0189f1e6e0803ce00747254b1e8ed
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-03-19 17:43:08 -0400

    refactor: Create a game grid for two boards

--- src/modules/dom/gameGrid.js
+++ src/modules/dom/gameGrid.js
@@ -1,13 +1,14 @@
 import { Gameboard } from "../classes/gameboard";
 
 export function createGameGrid() {
-    const grid = document.querySelector('.game-grid');
+    const [gridOne, gridTwo] = document.querySelectorAll('.game-grid');
 
     for (let i = Gameboard.BOUNDS.ROW.START; i <= Gameboard.BOUNDS.ROW.END; ++i) {
         for (let j = Gameboard.BOUNDS.COL.START; j <= Gameboard.BOUNDS.COL.END; ++j) {
             const div = document.createElement('div');
             div.dataset.coordinate = `${String.fromCharCode(j)}${i}`;
-            grid.appendChild(div);
+            gridOne.appendChild(div.cloneNode(true));
+            gridTwo.appendChild(div.cloneNode(true));
         }
     }
 }

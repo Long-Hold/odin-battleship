b075623c43f3b94a653071588cdb383297d6600c
Matthew <102175343+Long-Hold@users.noreply.github.com>
2026-03-24 16:01:48 -0400

    feat: Add a method that randomly places ships onto the grid

--- src/modules/classes/player.js
+++ src/modules/classes/player.js
@@ -1,7 +1,53 @@
 import { Gameboard } from "./gameboard";
+import { Ship } from "./ship";
 
 export class Player {
     constructor() {
         this.gameBoard = new Gameboard();
+    }
+
+    /**
+     * Places each ship onto a random range of coordinates.
+     * 
+     * @returns {this} An instance of the object for chaining.
+     */
+    randomizeShipPlacements() {
+        /**
+         * The while loop increments a random axis from a randomly generated
+         * coordinated. There are no bounds checking during this so it can make
+         * and invalid range.
+         * 
+         * In that case, it just recalculates. This is a brute force approach, but
+         * the size of the grid means this won't happen too often.
+         */
+        for (const [ship, length] of Ship.TYPES) {
+            while (true) {
+                const coordinate = Gameboard.getRandomCoordinate();
+
+                const letter = coordinate[0];
+                const number = parseInt(coordinate.slice(1));
+
+                const randomAxis = Math.random() > 0.5 ? number : letter;
+                const coordRange = [];
+
+                if (Number.isInteger(randomAxis))
+                    for (let i = 0; i < length; ++i)
+                        coordRange.push(`${letter}${randomAxis + i}`);
+                else {
+                    const charCode = letter.charCodeAt();
+                    for (let i = 0; i < length; ++i)
+                        coordRange.push(`${String.fromCharCode(charCode + i)}${number}`);
+                }
+
+                try {
+                    this.gameBoard.placeShip(coordRange, ship);
+                    break;
+                } catch {
+                    // Swallow errors and recalculate
+                }
+            }
+        }
+
+        return this;
     }
 }
--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,5 +1,6 @@
 import { Gameboard } from "../modules/classes/gameboard";
 import { Player } from "../modules/classes/player";
+import { Ship } from "../modules/classes/ship";
 
 describe('class Player', () => {
     let player;
@@ -10,6 +11,18 @@ describe('class Player', () => {
     describe('constructor', () => {
         test('Player object owns an instance of a Gameboard object', () => {
             expect(player.gameBoard).toBeInstanceOf(Gameboard);
+        });
+    });
+
+    describe('Player.randomizeShipPlacements()', () => {
+        test('Player.gameBoard.placeShip() throws an error when trying to place duplicates after method finishes', () => {
+            player.randomizeShipPlacements();
+            for (const [ship, length] of Ship.TYPES) {
+                const coords = [];
+                for (let i = 1; i <= length; ++i)
+                    coords.push(`A${i}`);
+                expect(() => player.gameBoard.placeShip(coords, ship)).toThrow(Error);
+            }
         });
     });
 });

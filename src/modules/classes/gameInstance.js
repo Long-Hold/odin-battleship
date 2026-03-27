202155cb0e16e0b94fe6787009e30da5295e44a0
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-03-26 23:45:20 -0400

    feat: Add handleAttack method and test suite for class

--- src/modules/classes/gameInstance.js
+++ src/modules/classes/gameInstance.js
@@ -10,4 +10,36 @@ export class Game {
             board: gameBoardTwo,
         }
     }
+
+    handleAttack(coordinate) {
+        const playerOneTurnCount = this.playerOne.player.gameBoard.guessedSpaces.size;
+        const playerTwoTurnCount = this.playerTwo.player.gameBoard.guessedSpaces.size;
+
+        /**
+         * THe player that gets to make a turn can be determined by how many turns they have taken.
+         * 
+         * If both players have the same amount of turns, then player 1 places the next attack.
+         * Otherwise, player 2 goes.
+         */
+
+        /**
+         * TODO:
+         *  - Record the return value of receiveAttack to tell DOM what icon to use on grid (hit or miss)
+         */
+        if (playerOneTurnCount === playerTwoTurnCount) {
+            try {
+                this.playerOne.player.gameBoard.recordPlacedAttack(coordinate);
+                this.playerTwo.player.gameBoard.receiveAttack(coordinate);
+            } catch (error) {
+                console.error(error);
+            }
+        } else {
+            try {
+                this.playerTwo.player.gameBoard.recordPlacedAttack(coordinate);
+                this.playerOne.player.gameBoard.receiveAttack(coordinate);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    }
 }

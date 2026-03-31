70c49883187dafaabfd3d7bb9f2e0c9084cd6ac8
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-03-31 15:51:43 -0400

    refactor: Implement basic game initialization logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-import { assignGameGridIDs, createGameGrid } from "./modules/dom/gameGrid";
+import { assignGameGridIDs, createGameGrid, displayShips, GRID_IDS } from "./modules/dom/gameGrid";
+import { Player, Computer } from "./modules/classes/player";
+import { Game } from "./modules/classes/gameInstance"
 import "./styles/styles.css";
 
-createGameGrid();
-assignGameGridIDs();
+function initializeGame() {
+    const playerOne = new Player();
+    const playerTwo = new Computer();
+
+    playerOne.randomizeShipPlacements();
+    playerTwo.randomizeShipPlacements();
+
+    createGameGrid();
+    assignGameGridIDs();
+
+    const game = new Game(
+        playerOne, document.querySelector(`#${GRID_IDS.PLAYER_ONE}`), 
+        playerTwo, document.querySelector(`#${GRID_IDS.COMPUTER}`)
+    );
+    displayShips(game.playerOne.board, playerOne.gameBoard.shipPlacements);
+    displayShips(game.playerTwo.board, playerTwo.gameBoard.shipPlacements);
+}
+
+initializeGame();

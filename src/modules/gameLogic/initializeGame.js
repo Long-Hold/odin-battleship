676ac3bcdf2566ecf26fa65f1c92c619e55b4155
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-03-31 18:08:38 -0400

    feat: Create a dedicated module for game initialization

---
+++ src/modules/gameLogic/initializeGame.js
@@ -0,0 +1,35 @@
+import { Game } from "../classes/gameInstance";
+import { Computer, Player } from "../classes/player";
+import { assignGameGridIDs, createGameGrid, GRID_IDS } from "../dom/gameGrid";
+
+/**
+ * Initializes the UI DOM elements and internal objects required
+ * for gameplay in the following order:
+ * 
+ * 1. Creates the game grids.
+ * 2. Assigns ID's to the game grid elements.
+ * 3. Creates Player instances.
+ * 4. Creates a Game instance to link Player's with their DOM gameboards.
+ * 
+ * @param {boolean} hasComputerPlayer - True if Player 2 is a CPU. False is not. 
+ * @returns {object} The created Game class instance.
+ */
+export function initializeGame(hasComputerPlayer = true) {
+    createGameGrid();
+    assignGameGridIDs(hasComputerPlayer);
+
+    const playerOne = new Player();
+    const playerTwo = hasComputerPlayer ? new Computer() : new Player();
+
+    const playerOneBoard = document.querySelector(`#${GRID_IDS.PLAYER_ONE}`);
+    const playerTwoBoard = document.querySelector(
+        hasComputerPlayer ? `#${GRID_IDS.COMPUTER}` : `#${GRID_IDS.PLAYER_TWO}`
+    );
+
+    const game = new Game(
+        playerOne, playerOneBoard,
+        playerTwo, playerTwoBoard
+    );
+
+    return game;
+}

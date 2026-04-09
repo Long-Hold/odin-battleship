be18d876a53d38c49173be3c3f745a1275a35a2d
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-04-09 16:31:07 -0400

    feat: Add basic game running logic

--- src/modules/gameLogic/runGame.js
+++ src/modules/gameLogic/runGame.js
@@ -1,7 +1,22 @@
 export async function runGame(game, coordinateRetriever) {
+    const playerOne = game.playerOne.player;
+    const playerTwo = game.playerTwo.player;
+
     while (true) {
         console.log('waiting for click...');
         const coord = await coordinateRetriever();
         console.log(coord);
+
+        try {
+            game.handleAttack(coord);
+        } catch (error) {
+            console.log('invalid');
+            continue;
+        }
+
+        if (playerOne.gameBoard.allShipsSunk() || playerTwo.gameBoard.allShipsSunk()) {
+            alert('Game over! Loser:', playerOne.gameBoard.allShipsSunk() ? playerOne : playerTwo);
+            break;
+        }
     }
 }

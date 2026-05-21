bbf2d32b0fc6f4f61e82097b29dfd267c128b094
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-05-21 15:27:59 -0400

    feat: call initializeGridResizeObserver() in start()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { waitForIntroClick } from "./modules/dom/eventListeners";
+import { initializeGridResizeObserver } from "./modules/dom/gameSprites";
 import { initializeGame, initializeGameButtons } from "./modules/gameLogic/initializeGame";
 import "./styles/styles.css";
 
@@ -6,6 +7,7 @@ async function start() {
     await waitForIntroClick();
     const game = initializeGame();
     initializeGameButtons(game);
+    initializeGridResizeObserver(game);
 }
 
 start();
